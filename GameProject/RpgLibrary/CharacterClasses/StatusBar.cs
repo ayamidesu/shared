6a8a9b9be51d57da8970ac10c24859fdd2f55f7c
Nancy <sdi0900126@di.uoa.gr>
2013-03-10 17:39:37 +0000

    Starting point

---
+++ GameProject/RpgLibrary/CharacterClasses/StatusBar.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RpgLibrary.CharacterClasses
+{
+    public class StatusBar
+    {
+        #region Field Region
+
+        int currentValue;
+        int maximumValue;
+
+        #endregion
+
+        #region Property Region
+
+        public int CurrentValue
+        {
+            get { return currentValue; }
+        }
+
+        public int MaximumValue
+        {
+            get { return maximumValue; }
+        }
+
+        public static StatusBar Zero
+        {
+            get { return new StatusBar(); }
+        }
+
+        #endregion
+
+        #region Constructor Region
+
+        private StatusBar()
+        {
+            currentValue = 0;
+            maximumValue = 0;
+        }
+
+        public StatusBar(int maxValue)
+        {
+            currentValue = maxValue;
+            maximumValue = maxValue;
+        }
+
+        #endregion
+
+        #region Method Region
+
+        public void Heal(ushort value)
+        {
+            currentValue += value;
+            if (currentValue > maximumValue)
+                currentValue = maximumValue;
+        }
+
+        public void Damage(ushort value)
+        {
+            currentValue -= value;
+            if (currentValue < 0)
+                currentValue = 0;
+        }
+
+        public void SetCurrent(int value)
+        {
+            currentValue = value;
+            if (currentValue > maximumValue)
+                currentValue = maximumValue;
+        }
+
+        public void SetMaximum(int value)
+        {
+            maximumValue = value;
+            if (currentValue > maximumValue)
+                currentValue = maximumValue;
+        }
+
+        #endregion
+    }
+}
